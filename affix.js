300bd7136909f24de93e6b2945067acc7a05af33
Lingyun Guo <l35guo@uwaterloo.ca>
2016-09-19 23:54:47 -0400

    Remove unnecessary comment.

--- affix.js
+++ affix.js
@@ -34,38 +34,3 @@ $(document).ready(function () {
     }
 
 });
-    // function onScroll() {
-    //     var scroll_px = $(window).scrollTop();
-    //     var col3_width = $(".floating_nav").width();
-    //     if ($(window).scrollTop() > 1330) {
-    //         $(".floating_nav").css({
-    //             display: "block"
-    //         });
-    //         $(".floating_nav > ul").css({
-    //             width: col3_width
-    //         });
-    //     }
-    //     else {
-    //         $(".floating_nav").css({
-    //             display: "none"
-    //         });
-    //     }
-    //     for (var i = 0; i < px_list.length; i++) {
-    //         if (scroll_px > px_list[i]) {
-    //             if (i === px_list.length - 1) {
-    //                 $(".floating_nav .active").removeClass("active");
-    //                 $(".floating_nav a[href='#article_12']").addClass("active");
-    //             }
-    //             continue;
-    //         }
-    //         if (scroll_px < px_list[i]) {
-    //             $(".floating_nav .active").removeClass("active");
-    //             if (i === 0) {
-    //                 break;
-    //             }
-    //             $(".floating_nav a[href='#article_" + (i - 1) + "']").addClass("active");
-    //             break;
-    //         }
-    //     }
-    // }
-// });
