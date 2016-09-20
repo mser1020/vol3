4862cf146bb55596f4c13b2039fff1dea95d7abf
Lingyun Guo <l35guo@uwaterloo.ca>
2016-09-19 23:54:12 -0400

    Fix scroll bug.

--- affix.js
+++ affix.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     });
     onScroll();
     function onScroll() {
-        var scroll_px = $(window).scrollTop();
         var col3_width = $(".floating_nav").width();
         if ($(window).scrollTop() > 1330) {
             $(".floating_nav").css({
@@ -20,22 +19,53 @@ $(document).ready(function () {
                 display: "none"
             });
         }
-        for (var i = 0; i < px_list.length; i++) {
-            if (scroll_px > px_list[i]) {
-                if (i === px_list.length - 1) {
-                    $(".floating_nav .active").removeClass("active");
-                    $(".floating_nav a[href='#article_12']").addClass("active");
-                }
-                continue;
+        $("div[id^='article_']").each(function () {
+            var top_of_element = $(this).offset().top;
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var top_of_viewport = $(window).scrollTop();
+            var bottom_of_viewport = $(window).scrollTop() + $(window).height();
+            if ((top_of_viewport + 50 > top_of_element) && (bottom_of_element < bottom_of_viewport + 50)) {
+                // The element is visible, do something
+                var id = $(this).attr("id");
+                $(".floating_nav a.active").removeClass("active");
+                $(".floating_nav a[href='#" + id + "']").addClass("active");
             }
-            if (scroll_px < px_list[i]) {
-                $(".floating_nav .active").removeClass("active");
-                if (i === 0) {
-                    break;
-                }
-                $(".floating_nav a[href='#article_" + (i - 1) + "']").addClass("active");
-                break;
-            }
-        }
+        });
     }
+
 });
+    // function onScroll() {
+    //     var scroll_px = $(window).scrollTop();
+    //     var col3_width = $(".floating_nav").width();
+    //     if ($(window).scrollTop() > 1330) {
+    //         $(".floating_nav").css({
+    //             display: "block"
+    //         });
+    //         $(".floating_nav > ul").css({
+    //             width: col3_width
+    //         });
+    //     }
+    //     else {
+    //         $(".floating_nav").css({
+    //             display: "none"
+    //         });
+    //     }
+    //     for (var i = 0; i < px_list.length; i++) {
+    //         if (scroll_px > px_list[i]) {
+    //             if (i === px_list.length - 1) {
+    //                 $(".floating_nav .active").removeClass("active");
+    //                 $(".floating_nav a[href='#article_12']").addClass("active");
+    //             }
+    //             continue;
+    //         }
+    //         if (scroll_px < px_list[i]) {
+    //             $(".floating_nav .active").removeClass("active");
+    //             if (i === 0) {
+    //                 break;
+    //             }
+    //             $(".floating_nav a[href='#article_" + (i - 1) + "']").addClass("active");
+    //             break;
+    //         }
+    //     }
+    // }
+// });
